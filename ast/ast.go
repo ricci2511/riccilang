ecef20577c51da97b7d7ca38fc609e35bd1ee053
ricci2511 <ricardo.christmann@protonmail.com>
2023-12-28 17:38:06 +0100

    add String method to ast.Node interface

--- ast/ast.go
+++ ast/ast.go
@@ -1,10 +1,15 @@
 package ast
 
-import "github.com/ricci2511/riccilang/token"
+import (
+	"bytes"
+
+	"github.com/ricci2511/riccilang/token"
+)
 
 // The base interface that all AST nodes implement
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // An AST node that doesn't produce a value
@@ -31,6 +36,16 @@ func (p *Program) TokenLiteral() string {
 	return ""
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, stmt := range p.Statements {
+		out.WriteString(stmt.String())
+	}
+
+	return out.String()
+}
+
 // The expression node that represents the identifier of a binding
 type Identifier struct {
 	Token token.Token // The token.IDENT token
@@ -39,6 +54,7 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
 
 // The statement node that represents a let binding (e.g. let x = 5;)
 type LetStatement struct {
@@ -47,8 +63,24 @@ type LetStatement struct {
 	Value Expression
 }
 
-func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) statementNode() {}
+
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
 
 // The statement node that represents a return statement (e.g. return 5;)
 type ReturnStatement struct {
@@ -56,5 +88,36 @@ type ReturnStatement struct {
 	ReturnValue Expression
 }
 
-func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) statementNode() {}
+
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
+// The statement node that represents an expression statement (e.g. 5 + 5;)
+type ExpressionStatement struct {
+	Token token.Token // The first token of the expression
+	Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
